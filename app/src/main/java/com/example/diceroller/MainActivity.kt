be4bd70dc5e280483d32991712dd3b9b86b0b7f2
Sayantan <sayantan.dasgupta21@gmail.com>
2021-11-06 00:07:30 +0530

    Added some interactivity

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -22,10 +22,22 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(6)
 
         val diceRoll = dice.roll()
+        val luckyNumber = 4
 
         val resultTextView: TextView = findViewById(R.id.textView)
+        val resultMessageTextView : TextView = findViewById(R.id.messageTextView)
 
         resultTextView.text = "$diceRoll"
+
+        resultMessageTextView.text = when(diceRoll) {
+            luckyNumber -> "You Won!"
+            1 -> "So sorry! You rolled a 1. Try again!"
+            2 -> "Sadly, you rolled a 2. Try again!"
+            3 -> "Unfortunately, you rolled a 3. Try again!"
+            5 -> "Don't cry! You rolled a 5. Try again!"
+            6 -> "Apologies! you rolled a 6. Try again!"
+            else -> "Result"
+        }
     }
 }
 
